1ac0b47c7c4cccb8f24ccc36f7d8e801f6f143c3
Nisreen <nwael@itgsoftware.com>
2023-01-18 13:00:23 +0200

    update

--- frontend/applepay.js
+++ frontend/applepay.js
@@ -15,125 +15,126 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         });
     }
-}, false);
 
-//Handel the apple pay button click
-appleButton.addEventListener('click', function() {
-    console.log('ApplePayButton Clicked');
-    const request = {
-        countryCode: 'US',
-        currencyCode: 'USD',
-        merchantCapabilities: [
-            'supports3DS',
-            'supportsDebit',
-            'supportsCredit',
-        ],
-        supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
-        total: { 
-            label: 'Product Name', 
-            amount: '1',
-            type: 'final'
-        }
-    };
-
-    console.log('Apple Pay request:', request);
-
-    const applePaySession = new ApplePaySession(3, request);
-    console.log('Apple Pay Session:', session);
-
-    //This is the first event that apple triggers. Here you need to validate the
-    //Apple pay session from Backend
-    applePaySession.onvalidatemerchant = async (event) => {
-        console.log('onvalidatemerchant:', event);
-
-        const merchantSession = await axios.post(
-            `${NGROK_HTTPS_HOST}/validate-merchant`,
-            {
-                appleUrl: event.validationURL
-            }
-        );
-        console.log(
-            'onvalidatemerchant (checkout) merchantSession:',
-            merchantSession
-        );
-        session.completeMerchantValidation(merchantSession);
-    };
-
-    // Define ApplePayPaymentMethodUpdate based on the selected payment method.
-    // No updates or errors are needed, pass an empty object.
-    applePaySession.onpaymentmethodselected = (event) => {
-        console.log('onpaymentmethodselected:', event);
-        const update = {};
-        applePaySession.completePaymentMethodSelection(update);
-    };
-
-    // Define ApplePayShippingMethodUpdate based on the selected shipping method.
-    // No updates or errors are needed, pass an empty object.
-    applePaySession.onshippingmethodselected = (event) => {
-        console.log('onshippingmethodselected:', event);
-        const update = {};
-        applePaySession.completeShippingMethodSelection(update);
-    };
-
-    // Define ApplePayShippingContactUpdate based on the selected shipping contact.
-    applePaySession.onshippingcontactselected = (event) => {
-        console.log('onshippingcontactselected:', event);
-        const update = {};
-        applePaySession.completeShippingContactSelection(update);
-    };
-
-    //This triggers after the user  has confirmed the transaction with the Touch ID or Face
-    //This will contain the payment token
-    applePaySession.onpaymentauthorized = async (event) => {
-        console.log('onpaymentauthorized event:', event);
-
-        const lineItems = {
-            lineItem: {
-                itemId: '12345',
-                name: 'prod1',
-                quantity: '2',
-                unitPrice: '456'
-            }
-        };
-        const body = {
-            transactionRequest: {
-                transactionType: 'authCaptureTransaction',
+    
+    //Handel the apple pay button click
+    appleButton.addEventListener('click', function() {
+        console.log('ApplePayButton Clicked');
+        const request = {
+            countryCode: 'US',
+            currencyCode: 'USD',
+            merchantCapabilities: [
+                'supports3DS',
+                'supportsDebit',
+                'supportsCredit',
+            ],
+            supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
+            total: { 
+                label: 'Product Name', 
                 amount: '1',
-                payment: {
-                opaqueData: {
-                    dataDescriptor: 'COMMON.APPLE.INAPP.PAYMENT',
-                    dataValue: event.token.paymentData.data,
-                    // "1234567890ABCDEF1111AAAA2222BBBB3333CCCC4444DDDD5555EEEE6666FFFF7777888899990",
-                },
-                },
-                lineItems,
-            },
+                type: 'final'
+            }
         };
 
-        const {
-            data: { transactionResponse, messages },
-        } = await axios.post(`${NGROK_HTTPS_HOST}/authorizeNetApi`, body);
+        console.log('Apple Pay request:', request);
 
-        let result = '';
-        if (messages.resultCode && messages.resultCode === 'Ok') {
-            result = {
-                status: ApplePaySession.STATUS_SUCCESS,
+        const applePaySession = new ApplePaySession(3, request);
+        console.log('Apple Pay Session:', session);
+
+        //This is the first event that apple triggers. Here you need to validate the
+        //Apple pay session from Backend
+        applePaySession.onvalidatemerchant = async (event) => {
+            console.log('onvalidatemerchant:', event);
+
+            const merchantSession = await axios.post(
+                `${NGROK_HTTPS_HOST}/validate-merchant`,
+                {
+                    appleUrl: event.validationURL
+                }
+            );
+            console.log(
+                'onvalidatemerchant (checkout) merchantSession:',
+                merchantSession
+            );
+            session.completeMerchantValidation(merchantSession);
+        };
+
+        // Define ApplePayPaymentMethodUpdate based on the selected payment method.
+        // No updates or errors are needed, pass an empty object.
+        applePaySession.onpaymentmethodselected = (event) => {
+            console.log('onpaymentmethodselected:', event);
+            const update = {};
+            applePaySession.completePaymentMethodSelection(update);
+        };
+
+        // Define ApplePayShippingMethodUpdate based on the selected shipping method.
+        // No updates or errors are needed, pass an empty object.
+        applePaySession.onshippingmethodselected = (event) => {
+            console.log('onshippingmethodselected:', event);
+            const update = {};
+            applePaySession.completeShippingMethodSelection(update);
+        };
+
+        // Define ApplePayShippingContactUpdate based on the selected shipping contact.
+        applePaySession.onshippingcontactselected = (event) => {
+            console.log('onshippingcontactselected:', event);
+            const update = {};
+            applePaySession.completeShippingContactSelection(update);
+        };
+
+        //This triggers after the user  has confirmed the transaction with the Touch ID or Face
+        //This will contain the payment token
+        applePaySession.onpaymentauthorized = async (event) => {
+            console.log('onpaymentauthorized event:', event);
+
+            const lineItems = {
+                lineItem: {
+                    itemId: '12345',
+                    name: 'prod1',
+                    quantity: '2',
+                    unitPrice: '456'
+                }
             };
-            document.getElementById('success').style.display = 'block';
-        } else {
-            console.log('error transactionResponse:', transactionResponse);
-            console.log('error messages:', messages);
-            result = {
-                status: ApplePaySession.STATUS_FAILURE,
+            const body = {
+                transactionRequest: {
+                    transactionType: 'authCaptureTransaction',
+                    amount: '1',
+                    payment: {
+                    opaqueData: {
+                        dataDescriptor: 'COMMON.APPLE.INAPP.PAYMENT',
+                        dataValue: event.token.paymentData.data,
+                        // "1234567890ABCDEF1111AAAA2222BBBB3333CCCC4444DDDD5555EEEE6666FFFF7777888899990",
+                    },
+                    },
+                    lineItems,
+                },
             };
-        }
-        applePaySession.completePayment(result);
-    };
 
-    // Payment cancelled by WebKit
-    applePaySession.oncancel = (event) => {
-        console.log('oncancel:', event);
-    };
+            const {
+                data: { transactionResponse, messages },
+            } = await axios.post(`${NGROK_HTTPS_HOST}/authorizeNetApi`, body);
 
-    applePaySession.begin();
-});
+            let result = '';
+            if (messages.resultCode && messages.resultCode === 'Ok') {
+                result = {
+                    status: ApplePaySession.STATUS_SUCCESS,
+                };
+                document.getElementById('success').style.display = 'block';
+            } else {
+                console.log('error transactionResponse:', transactionResponse);
+                console.log('error messages:', messages);
+                result = {
+                    status: ApplePaySession.STATUS_FAILURE,
+                };
+            }
+            applePaySession.completePayment(result);
+        };
+
+        // Payment cancelled by WebKit
+        applePaySession.oncancel = (event) => {
+            console.log('oncancel:', event);
+        };
+
+        applePaySession.begin();
+    });
+}, false);
