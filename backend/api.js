0a631208bca1824d0959c81e8915195d439bb6f3
Nisreen <nwael@itgsoftware.com>
2023-01-18 11:38:17 +0200

    update the code

---
+++ backend/api.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const https = require('https');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/validate-merchant', async (req, res)  => { 
+    try {
+        const appleUrl = req?.body?.appleUrl || 'https://apple-pay-gateway.apple.com/paymentservices/startSession';
+        // use set the certificates for the POST request
+        httpsAgent = new https.Agent({
+            rejectUnauthorized: false,
+            cert: fs.readFileSync(path.join(__dirname, './certs/marchent_id.pem')),
+            key: fs.readFileSync(path.join(__dirname, './certs/marchent_id.key')),
+        });
+    
+        response = await axios.post(
+            appleUrl,
+            {
+                merchantIdentifier: 'merchant.apple.itg.test',
+                domainName: 'apple-pay-g4qy5.ondigitalocean.app',
+                displayName: 'apple-pay-g4qy5.ondigitalocean.app',
+            },
+            {
+                httpsAgent,
+            }
+        );
+        res.send(response?.data);
+    } catch (error) {
+        console.log('error', error);
+    }
+});
+
+app.post('/authorizeNetApi', async (req, res)  => { 
+    const reqData = {
+        createTransactionRequest: {
+            merchantAuthentication: {
+            name: '39PR8MKxrTA', //NEXT_AUTHORIZENET_API_LOGIN_ID,
+            transactionKey: '5W6zqL84KcK8A4kS' //NEXT_AUTHORIZENET_TRANSACTION_KEY,
+            },
+            ...req?.body,
+        },
+    };
+    let authNetConfig = {
+        method: 'post',
+        url: 'https://apitest.authorize.net/xml/v1/request.api', //NEXT_AUTHORIZENET_API_URL
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        data: reqData,
+    };
+    try {
+        const { data } = await axios(authNetConfig);
+        res.status(200).json(data);
+        return;
+    } catch (err) {
+        res
+        .status(err?.response?.status || 500)
+        .json(err && err.response && err.response.data);
+        return;
+    }
+});
+
+app.listen(port, function() {
+    console.log('Apple Pay server running on ' + port);
+});
---
+++ frontend/applepay.js
@@ -0,0 +1,140 @@
+const axios = require('axios');
+var MERCHANT_IDENTIFIER = 'merchant.apple.itg.test';
+const NGROK_HTTPS_HOST = 'https://1e83338f.ngrok.io';
+
+var appleButton = document.querySelector('.apple-pay-button');
+
+//check if the apple pay is available
+document.addEventListener('DOMContentLoaded', function() { 
+    if (window.ApplePaySession && ApplePaySession.canMakePayments()) {
+        var promise = ApplePaySession.canMakePaymentsWithActiveCard(MERCHANT_IDENTIFIER);
+        promise.then(function(canMakePayments) {
+        if (canMakePayments) {
+            console.log('canMakePayments(): true');
+            //show the apple pay button
+            appleButton.style.display = 'block';
+        }
+        });
+    }
+}, false);
+
+//Handel the apple pay button click
+document.addEventListener('click', function() {
+    console.log('ApplePayButton Clicked');
+    const request = {
+        countryCode: 'US',
+        currencyCode: 'USD',
+        merchantCapabilities: [
+            'supports3DS',
+            'supportsDebit',
+            'supportsCredit',
+        ],
+        supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
+        total: { 
+            label: 'Product Name', 
+            amount: '1',
+            type: 'final'
+        }
+    };
+
+    console.log('Apple Pay request:', request);
+
+    const applePaySession = new ApplePaySession(3, request);
+    console.log('Apple Pay Session:', session);
+
+    //This is the first event that apple triggers. Here you need to validate the
+    //Apple pay session from Backend
+    applePaySession.onvalidatemerchant = async (event) => {
+        console.log('onvalidatemerchant:', event);
+
+        const merchantSession = await axios.post(
+            `${NGROK_HTTPS_HOST}/validate-merchant`,
+            {
+                appleUrl: event.validationURL
+            }
+        );
+        console.log(
+            'onvalidatemerchant (checkout) merchantSession:',
+            merchantSession
+        );
+        session.completeMerchantValidation(merchantSession);
+    };
+
+    // Define ApplePayPaymentMethodUpdate based on the selected payment method.
+    // No updates or errors are needed, pass an empty object.
+    applePaySession.onpaymentmethodselected = (event) => {
+        console.log('onpaymentmethodselected:', event);
+        const update = {};
+        applePaySession.completePaymentMethodSelection(update);
+    };
+
+    // Define ApplePayShippingMethodUpdate based on the selected shipping method.
+    // No updates or errors are needed, pass an empty object.
+    applePaySession.onshippingmethodselected = (event) => {
+        console.log('onshippingmethodselected:', event);
+        const update = {};
+        applePaySession.completeShippingMethodSelection(update);
+    };
+
+    // Define ApplePayShippingContactUpdate based on the selected shipping contact.
+    applePaySession.onshippingcontactselected = (event) => {
+        console.log('onshippingcontactselected:', event);
+        const update = {};
+        applePaySession.completeShippingContactSelection(update);
+    };
+
+    //This triggers after the user  has confirmed the transaction with the Touch ID or Face
+    //This will contain the payment token
+    applePaySession.onpaymentauthorized = async (event) => {
+        console.log('onpaymentauthorized event:', event);
+
+        const lineItems = {
+            lineItem: {
+                itemId: '12345',
+                name: 'prod1',
+                quantity: '2',
+                unitPrice: '456'
+            }
+        };
+        const body = {
+            transactionRequest: {
+                transactionType: 'authCaptureTransaction',
+                amount: '1',
+                payment: {
+                opaqueData: {
+                    dataDescriptor: 'COMMON.APPLE.INAPP.PAYMENT',
+                    dataValue: event.token.paymentData.data,
+                    // "1234567890ABCDEF1111AAAA2222BBBB3333CCCC4444DDDD5555EEEE6666FFFF7777888899990",
+                },
+                },
+                lineItems,
+            },
+        };
+
+        const {
+            data: { transactionResponse, messages },
+        } = await axios.post('/authorizeNetApi', body);
+
+        let result = '';
+        if (messages.resultCode && messages.resultCode === 'Ok') {
+            result = {
+                status: ApplePaySession.STATUS_SUCCESS,
+            };
+            document.getElementById('success').style.display = 'block';
+        } else {
+            console.log('error transactionResponse:', transactionResponse);
+            console.log('error messages:', messages);
+            result = {
+                status: ApplePaySession.STATUS_FAILURE,
+            };
+        }
+        applePaySession.completePayment(result);
+    };
+
+    // Payment cancelled by WebKit
+    applePaySession.oncancel = (event) => {
+        console.log('oncancel:', event);
+    };
+
+    applePaySession.begin();
+});
