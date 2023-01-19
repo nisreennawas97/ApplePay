73fb82e57aacd7660ec5378cd1f7a4b2482ed274
Nisreen <nwael@itgsoftware.com>
2023-01-19 13:21:08 +0200

    update

--- frontend/applepay.js
+++ frontend/applepay.js
@@ -85,7 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
         //This will contain the payment token
         applePaySession.onpaymentauthorized = async (event) => {
             console.log('onpaymentauthorized event:', event);
-            const token = btoa(event.payment.token.paymentData.data);
+            const token = btoa(event.payment.token.paymentData);
             console.log('token value', token);
 
             const lineItems = {
